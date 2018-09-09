d168a15fbe826e9c06ccb7c582977306e53e768a
Bookyoulove <djdjdj18@naver.com>
2018-09-09 22:56:35 +0900

    timeLine_
    type1

--- NewBlackOut/app/src/main/java/com/androidtown/blackout/TimeFragment.java
+++ NewBlackOut/app/src/main/java/com/androidtown/blackout/TimeFragment.java
@@ -1,22 +1,69 @@
 package com.androidtown.blackout;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class TimeFragment extends Fragment {
 
+    RecyclerView rcv;
+    LinearLayoutManager llm;
+    WrittingAdapter wadapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_time, container, false);
 
+
+        Activity activity = (MainActivity)getActivity();
+
+
+        //*1time위해 추가한것
+
+        rcv = (RecyclerView)view.findViewById(R.id.recycler_test);
+        llm = new LinearLayoutManager(activity);//종류는 총 3가지, ListView를 사용하기 위한 사용
+        rcv.setHasFixedSize(true);//각 아이템이 보여지는 것을 일정하게
+        rcv.setLayoutManager(llm);//앞서 선언한 리싸이클러뷰를 레이아웃메니저에 붙힌다
+
+
+        ArrayList<ItemForm> list = new ArrayList<>();//ItemFrom에서 받게되는 데이터를 어레이 리스트화 시킨다.
+
+        list.add(new ItemForm("19:30",R.drawable.kkko2,"카카오톡"));
+        list.add(new ItemForm("20:30",R.drawable.kkko2,"페이스북"));
+        list.add(new ItemForm("21:30",R.drawable.kkko2,"여자친구"));
+        list.add(new ItemForm("22:30",R.drawable.kkko2,"위치정보"));
+        list.add(new ItemForm("23:30",R.drawable.kkko2,"별타그램"));
+        list.add(new ItemForm("24:30",R.drawable.kkko2,"카카오톡"));
+        list.add(new ItemForm("25:30",R.drawable.kkko2,"카카오톡"));
+        list.add(new ItemForm("02:00",R.drawable.kkko2,"카카오톡"));
+        list.add(new ItemForm("02:01",R.drawable.kkko2,"카카오톡"));
+        list.add(new ItemForm("02:02",R.drawable.kkko2,"페이스북"));
+        list.add(new ItemForm("02:03",R.drawable.kkko2,"여자친구"));
+        list.add(new ItemForm("02:04",R.drawable.kkko2,"위치정보"));
+        list.add(new ItemForm("02:05",R.drawable.kkko2,"별타그램"));
+        list.add(new ItemForm("02:06",R.drawable.kkko2,"카카오톡"));
+        list.add(new ItemForm("02:07",R.drawable.kkko2,"카카오톡"));
+        list.add(new ItemForm("02:08",R.drawable.kkko2,"페이스북"));
+
+
+        wadapter = new WrittingAdapter(activity, list);
+
+        wadapter.notifyDataSetChanged();
+        rcv.setAdapter(wadapter);
+
+
         return view;
+
     }
 
 
---
+++ NewBlackOut/app/src/main/java/com/androidtown/blackout/WrittingAdapter.java
@@ -0,0 +1,81 @@
+package com.androidtown.blackout;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class WrittingAdapter extends RecyclerView.Adapter<WrittingAdapter.MyViewholder> {
+
+    private Activity activity;
+    private ArrayList<ItemForm> datalist;
+
+
+    //getItemCount, onCreateViewHolder, MyViewHolder, onBindViewholder 순으로 들어오게 된다.
+    // 뷰홀더에서 초기세팅해주고 바인드뷰홀더에서 셋텍스트해주는 값이 최종적으로 화면에 출력되는 값
+
+
+    @Override
+    public WrittingAdapter.MyViewholder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        MyViewholder viewholder1 = new MyViewholder(view);
+
+
+        return viewholder1;
+    }
+
+    @Override
+    public void onBindViewHolder(WrittingAdapter.MyViewholder holder, int position) {
+        ItemForm data = datalist.get(position);
+        //초기화 해볼려고 넣음 없어도 될 것 같음 다시 해봐야됨
+        holder.personalId.setText("");
+        holder.profile.setImageResource(0);
+        holder.writtingTxt.setText("");
+
+        holder.personalId.setText(data.getId());
+        holder.profile.setImageResource(data.getImageNumber());
+        holder.writtingTxt.setText(data.getTxt());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return datalist.size();
+    }
+
+    public class MyViewholder extends RecyclerView.ViewHolder {
+        ImageView profile;
+        TextView writtingTxt;
+        TextView personalId;
+
+        public MyViewholder(View itemview) {
+            super(itemview);
+
+            profile = (ImageView) itemview.findViewById(R.id.ivIcon);
+            writtingTxt = (TextView) itemview.findViewById(R.id.tvName);
+            personalId = (TextView) itemview.findViewById(R.id.tvTime);
+            itemview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(activity, "터치 받으면 펼쳐지거나 다른 뷰가 추가되서 넓어짐", Toast.LENGTH_LONG).show();
+                    ;
+                }
+            });
+
+        }
+
+    }
+
+    public WrittingAdapter(Activity activity, ArrayList<ItemForm> datalist) {
+        this.activity = activity;
+        this.datalist = datalist;
+        notifyDataSetChanged();
+
+    }
+}
